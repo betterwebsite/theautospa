17a106f908bd52186d97237d6e9db52bf54dc59c
betterwebsite <normanwebdesigners@gmail.com>
2025-03-13 14:34:28 -0500

    Initial Updates
    Jordan Babb

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,22 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "The Autospa LLC",
+    "email": "info@theautospallc.com",
+    "phoneForTel": "405-831-2000",
+    "phoneFormatted": "(405) 831-2000",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "1241 W Main St",
+        "lineTwo": "",
+        "city": "Oklahoma City",
+        "state": "OK",
+        "zip": "73106",
         "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "mapLink": "https://www.google.com/maps/dir//1241+W+Main+St,+Oklahoma+City,+OK+73106/@35.4681107,-97.6165172,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x87b210d0c79186cf:0xffac9c19ad36f3e8!2m2!1d-97.5341161!2d35.4681387?entry=ttu&g_ep=EgoyMDI1MDMxMC4wIKXMDSoASAFQAw%3D%3D"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "facebook": "https://www.facebook.com/theautospallc",
+        "instagram": "https://www.instagram.com/theautospallc/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    // https://www.flickr.com/photos/60446769@N04/
+    "domain": "https://www.theautospallc.com/"
 };
